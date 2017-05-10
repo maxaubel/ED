1c729c4c6e0a5edb926608451887327158145423
Maximiliano Aubel <maximiliano.aubel@sansano.usm.cl>
2017-05-10 08:41:12 -0300

    Actualización luego de tarea
    Actualizado: main, funciones.h, funciones.cpp

--- Tarea3/funciones.cpp
+++ Tarea3/funciones.cpp
@@ -5,82 +5,29 @@ using namespace std;
 #include <bitset>
 #include <string>
 #include <math.h> 
-#define MAX 30
+#define MAX 500
 
-
-
-
-void parte1()
+void NewProg()
 {
-	float P;
-	int L;
-	cin >> L;
-	int A[MAX];
-	for(int j=0; j < L; j++) cin >> A[j];
-	cin >> P;
-	Ordenar(A, L);
-	if (A[0] <= P && P <= A[L-1]) //Verifica, si es posible, sin tener que buscar coincidencias
-	{	cout << "condicion se cumple";
-		{	for(int i = L-1; 0 < i; i--) //Recorre el binario
-			{
-
-			}
-		}	
-	}
-	else cout << "0" << endl;
-	return;
-}
-
-////////////////////////////////////////////////////////////////
-int convertarraytoint( int array[], int limit ) {
-  int result = 0;
-  int invertarray[limit];
-  for (int j=0; j <= limit; j++){
-    invertarray[j] = array[limit-j];
-  }
-  for (int i=0; i <= limit; i++)
-  {
-    result *= 10;
-    result += invertarray[i];
-  }
-  return result;
-}
-
-int binary(int number) 
-{	int binarynum[1000];
-	int count = 0;
-	for ( int i = 0; number > 0 ; i ++ ) 
-	{	binarynum[i] = number % 2;
-		number = number / 2;
-		count++;
-	}
-
-	int finalbinary = convertarraytoint(binarynum,count);
-	return finalbinary;
-	
-}
-//////////////////////////////////////////////////////////////////////
-
-void parte2()
-{	cout<<"nada por ahora"<<endl;
-	return;
-}
-
-
-void Ordenar(int arr[], int n) //O(n^2)
-{	bool interc = true;
-	int j = 0;
-	int tmp;
-	while (interc) 
-	{	interc = false;
-		j++;
-		for (int i = 0; i < n - j; i++) 
-		{	if (arr[i] > arr[i + 1]) 
-			{	tmp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = tmp;
-				interc = true;
-			}
+	int NrProgramas = 0;
+	cin >> NrProgramas;
+	char A[NrProgramas][MAX];
+	for (int f = 0; f < NrProgramas; f++)
+	{	for (int C = 0; C < MAX; C++)
+		{	cin >> A[f][C];
+			if (A[f][C] == '!') break;
 		}
 	}
+
+	for (int f = 0; f < NrProgramas; f++)
+	{	for (int C = 0; C < MAX; C++)
+		{	cout << A[f][C]; 
+			if (A[f][C] == '!') break;
+		}
+		cout << endl;
+	} 
+	return;
 }
+
+
+
---
+++ Tarea3/subl
@@ -0,0 +1,12 @@
+#include "funciones.h"
+using namespace std;
+#include <iostream>
+#include <stdio.h>
+#include <bitset>
+#include <string>
+#include <math.h> 
+#define MAX 500
+
+int main(){
+	NewProg();
+}
